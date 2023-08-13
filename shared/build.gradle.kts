0a64ed4458afac6f0c44fa6f38b7c281978a4e0e
Nick Rafala <njrafala@outlook.com>
2023-08-13 17:05:27 -0400

    Add ktor and serialization dependencies for getting data from the internet

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("native.cocoapods")
     id("com.android.library")
     id("org.jetbrains.compose")
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 kotlin {
@@ -28,6 +29,8 @@ kotlin {
     }
 
     sourceSets {
+        val ktorVersion = "2.3.3"
+
         val commonMain by getting {
             dependencies {
                 implementation(compose.runtime)
@@ -35,20 +38,32 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
                 api(compose.animation)
 
-                val precomposeVersion="1.4.4"
+                // PreCompose
+                val precomposeVersion = "1.4.4"
                 api("moe.tlaster:precompose:$precomposeVersion")
                 api("moe.tlaster:precompose-molecule:$precomposeVersion") // For Molecule integration
                 api("moe.tlaster:precompose-viewmodel:$precomposeVersion") // For ViewModel integration
+
+                // ktor
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
             }
         }
         val androidMain by getting {
             dependencies {
-                api("androidx.activity:activity-compose:1.6.1")
+                api("androidx.activity:activity-compose:1.7.2")
                 api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.9.0")
+                api("androidx.core:core-ktx:1.10.1")
+
+                // use only one or none of the following
+                //implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
             }
         }
         val iosX64Main by getting
@@ -59,6 +74,9 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            }
         }
         val desktopMain by getting {
             dependencies {
