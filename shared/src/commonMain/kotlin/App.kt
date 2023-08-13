685f59a11e12aeca79e959dd41656d5ea1921fcb
Nick Rafala <njrafala@outlook.com>
2023-08-12 20:48:29 -0400

    Make a few pages to demonstrate navigation with PreCompose. Add top app bar for backwards navigation on desktop and iOS.

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -3,39 +3,87 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import moe.tlaster.precompose.navigation.NavHost
+import moe.tlaster.precompose.navigation.rememberNavigator
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
+    val navigator = rememberNavigator()
+    val isRootPage by navigator.canGoBack.collectAsState(initial = false)
+    val routes = listOf("/page1", "/page2", "/page3")
+
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
+        Scaffold(
+            topBar = {
+                TopAppBar(navigationIcon = {
+                    if (isRootPage) {
+                        IconButton(onClick = { navigator.goBack() }) {
+                            Icon(Icons.Filled.ArrowBack, "")
+                        }
+                    }
+                }, title = { Text("Pokedex") })
             }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
-                )
-            }
-        }
-    }
-}
+        ) {
+            NavHost(
+                navigator = navigator,
+                initialRoute = routes[0]
+            ) {
+                scene(route = routes[0]) {
+                    Column {
+                        Button(onClick = { navigator.navigate(routes[1]) }) { Text("To Page1") }
+                        Button(onClick = { navigator.navigate(routes[2]) }) { Text("To Page2") }
+                    }
+                } // scene
+                scene(route = routes[1]) {
+                    var greetingText by remember { mutableStateOf("Hello, World!") }
+                    var showImage by remember { mutableStateOf(false) }
+                    Column(
+                        Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            greetingText = "Hello, ${getPlatformName()}"
+                            showImage = !showImage
+                        }) {
+                            Text(greetingText)
+                        }
+                        AnimatedVisibility(showImage) {
+                            Image(
+                                painterResource("compose-multiplatform.xml"),
+                                null
+                            )
+                        }
+                    }
+                } // scene
+                scene(route = routes[2]) {
+                    var counter by remember { mutableStateOf(0) }
+                    Column {
+                        Text(text = counter.toString())
+                        Button(onClick = { counter += 1 }) { Text("Count") }
+                    }
+                } // scene
+            } // NavHost
+        } // Scaffold
+    } // MaterialTheme
+} // App
 
 expect fun getPlatformName(): String
